ee6f5f17966eeaafc52800dbecf46e6d3a60fb2f
tejabharathi <60387277+tejabharathi@users.noreply.github.com>
2020-01-29 13:44:41 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,6 +1,6 @@
 class A{
      public static void main(String b[]){
-         System.out.println("Wipro "+b[0]+" Banglore");
+         System.out.println("Wipro "+b[0]+"  Banglore");
 
        
     }
