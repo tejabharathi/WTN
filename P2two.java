97e9c1b5c6004d759ca2fa41f290c0d62b2267a2
tejabharathi <60387277+tejabharathi@users.noreply.github.com>
2020-01-29 13:52:40 +0530

    Add files via upload

---
+++ P2two.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class MyClass {
+   public static void main(String args[]) {
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         int c=0;
+         for(int i=2;i<=n/2;i++)
+            if(n%i==0)
+                c++;
+         if(c>0)
+            System.out.println("not prime");
+        else
+            System.out.println("prime");
+   }
+}
