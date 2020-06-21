6375cd1770ab8f85a669668fb39c662d12f8a3e3
tejabharathi <60387277+tejabharathi@users.noreply.github.com>
2020-06-21 21:06:05 +0530

    Add files via upload

---
+++ Truefalse.java
@@ -0,0 +1,20 @@
+
+public class Truefalse {
+	 public static void main(String args[])
+	 {
+		int a = Integer.parseInt(args[0]);
+		if(a>0)
+	{
+	  System.out.println("The Number "+a+" is Positive");
+	} 
+	else if (a<0)
+	{
+	System.out.println("The Number "+a+" is Negative");
+	}
+	else if(a==0)
+	{
+	System.out.println("The Number "+a+" is Equal To Zero");
+	}
+	  }
+
+}
