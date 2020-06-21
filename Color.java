e237630eef38314d17c4fe9d88c4e8b635d2513e
tejabharathi <60387277+tejabharathi@users.noreply.github.com>
2020-06-21 21:30:27 +0530

    Add files via upload

---
+++ Color.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class Color {
+	 public static void main(String args[])
+	 {	
+		String s=args[0];
+		char c=s.charAt(0);
+		switch (c) 
+		{
+	  	case 'R':
+	    		System.out.println("Red");
+	    		break;
+	  	case 'B':
+	    		System.out.println("Blue");
+	    		break;
+	  	case 'G':
+	    		System.out.println("Green");
+	    		break;
+	  	case 'O':
+	    		System.out.println("Orange");
+	    		break;
+	  	case 'Y':
+	    		System.out.println("Yellow");
+	    		break;
+	  	case 'W':
+	    		System.out.println("White");
+	    		break;
+		default:
+	    		System.out.println("Invalid Code");
+		}    		
+	 }
+
+}
