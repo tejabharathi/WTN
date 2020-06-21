11bcf3d4c839ca067f5c64d7adfb49e6380b3a48
tejabharathi <60387277+tejabharathi@users.noreply.github.com>
2020-06-21 21:10:35 +0530

    Add files via upload

---
+++ Evenorodd.java
@@ -0,0 +1,16 @@
+
+public class Evenorodd {
+	public static void main(String args[])
+	 {
+		int a = Integer.parseInt(args[0]);
+		if(a%2==0)
+	{
+	System.out.println("The Number "+a+"is Even ");
+	}
+	else
+	{
+	System.out.println("The Number "+a+"is Odd ");
+	}
+	  }
+
+}
