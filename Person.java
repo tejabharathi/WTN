3ebbdb566f4a71c4bee42477c20ff533ea59625f
tejabharathi <60387277+tejabharathi@users.noreply.github.com>
2020-06-28 20:43:03 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,22 @@
+public class Person
+{
+private String name;
+
+ public Person(String n)
+	{
+		name = n;
+	}
+ public void setName(String n)
+	{
+		name = n;
+	}
+ public String getName()
+	{
+		return name;
+	}
+ public String toString()
+	{
+		return name;
+	}
+
+}
---
+++ TestEmployee.java
@@ -0,0 +1,10 @@
+public class TestEmployee
+{
+public static void main(String args[])
+{
+Person p = new Person("Pavan");
+Employee e = new Employee("Pavan Kalyan",10000,2015,"abcd");
+System.out.println(p);
+System.out.println(e);
+}
+}
