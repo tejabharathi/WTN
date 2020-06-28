a7d737962828d5cb8a06527067868915e56208cb
tejabharathi <60387277+tejabharathi@users.noreply.github.com>
2020-06-28 20:41:05 +0530

    Add files via upload

---
+++ C4.java
@@ -0,0 +1,46 @@
+class Animal
+{
+void eat()
+{
+System.out.println("Animal Eat Method");
+}
+void sleep()
+{
+System.out.println("Animal Sleep Method");
+}
+}
+class Bird extends Animal
+{
+void eat()
+{
+System.out.println("Bird Eat Method");
+}
+void sleep()
+{
+System.out.println("Bird Sleep Method");
+}
+void fly()
+{
+System.out.println("Bird Fly Method");
+}
+}
+
+class C4
+{
+public static void main(String args[])
+{
+Animal a = new Animal();
+Bird b = new Bird();
+a.eat();
+a.sleep();
+b.eat();
+b.sleep();
+b.fly();
+}
+}
+
+
+
+
+
+
